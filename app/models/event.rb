907483fd38459121de5b1cd0fb5ecdf2aafcca17
werner <werner_a_e@yahoo.es>
2020-01-17 16:30:57 -0300

    Add validations for Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, presence: true
+  validate  :end_date_bigger_than_start_date
+
+  private
+
+  def end_date_bigger_than_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, 'must be bigger than start date')
+    end
+  end
 end
