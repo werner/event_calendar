5dbea22f3c1ded17421a5fa061048b8bfcde4189
werner <werner_a_e@yahoo.es>
2020-01-17 15:41:34 -0300

    Add Events Controller to handle index and create resources

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+    render json: @events
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:start, :end, :name)
+    end
+end
